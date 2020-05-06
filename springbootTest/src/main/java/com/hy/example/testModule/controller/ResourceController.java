4fd8a07b6387668ad46d023ac40c49154dd9fb0d
Administrator <Administrator@192.168.0.108>
2020-05-06 18:50:12 +0800

    "考试"

--- springbootTest/src/main/java/com/hy/example/testModule/controller/ResourceController.java
+++
@@ -1,6 +0,0 @@
-package com.hy.example.testModule.controller;
-
-public class ResourceController {
-	
-	
-}
--- springbootTest/src/main/java/com/hy/example/testModule/controller/RoleController.java
+++
@@ -1,5 +0,0 @@
-package com.hy.example.testModule.controller;
-
-public class RoleController {
-
-}
